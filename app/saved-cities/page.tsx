fc7637fa1bfd5e967251d66e2dddb3c88ac08b80
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-07-19 22:07:50 +0100

    update saved cities

--- app/saved-cities/page.tsx
+++ app/saved-cities/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation';
 
 const SavedCities = () => {
   const [savedCities, setSavedCities] = useState<string[]>([]);
+  const [filteredCities, setFilteredCities] = useState<string[]>([]);
+  const [selectedLetter, setSelectedLetter] = useState<string | null>(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -15,6 +17,7 @@ const SavedCities = () => {
       }
     }
     setSavedCities(cities);
+    setFilteredCities(cities);
   }, []);
 
   const handleCityClick = (city: string) => {
@@ -22,15 +25,35 @@ const SavedCities = () => {
     router.push('/dashboard');
   };
 
+  const handleLetterClick = (letter: string) => {
+    setSelectedLetter(letter);
+    const filtered = savedCities.filter(city => city.toLowerCase().startsWith(letter.toLowerCase()));
+    setFilteredCities(filtered);
+  };
+
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
   return (
     <div>
-      <h1 className="text-4xl font-montserrat mb-6 text-center">Saved Cities</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        {savedCities.map((city, index) => (
+      <h1 className="text-4xl font-montserrat mb-12 text-center">Saved Cities</h1>
+      <div className="text-center mb-4 px-20">
+        {alphabet.map(letter => (
+          <button
+            key={letter}
+            onClick={() => handleLetterClick(letter)}
+            className={`m-1 px-[0.55rem] py-[0.1rem] hover:bg-darkGrey hover:rounded-full hover:text-white ${selectedLetter === letter ? 'bg-transparent text-white hover:bg-transparent' : 'bg-white opacity-50 rounded-full text-black'}`}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+      <div className='flex flex-wrap justify-center mt-20 h-[50vh] items-center'>
+        {filteredCities.map((city, index) => (
           <div
             key={index}
             onClick={() => handleCityClick(city)}
-            style={{ border: '1px solid #ccc', margin: '10px', padding: '10px', width: '150px', cursor: 'pointer' }}
+            className='m-3 p-3 cursor-pointer w-40 text-center rounded-full uppercase hover:bg-[#ccc]'
+            style={{ border: '1px solid #ccc' }}
           >
             <p>{city}</p>
           </div>
