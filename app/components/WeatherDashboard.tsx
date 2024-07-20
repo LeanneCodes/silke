ae89ade83b17bdc0bf0680acb34ca865d1500c8e
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-07-20 11:44:19 +0100

    add extra info around dew point on dashboard

--- app/components/WeatherDashboard.tsx
+++ app/components/WeatherDashboard.tsx
@@ -22,7 +22,6 @@ const WeatherDashboard: React.FC<WeatherDashboardProps> = ({ cityName, currentWe
 
   useEffect(() => {
     localStorage.setItem('unit', unit);
-    console.log(`Saved unit to local storage: ${unit}`);
   }, [unit]);
 
   const convertToCelsius = (temp: number) => Math.round(temp - 273.15);
@@ -68,8 +67,60 @@ const WeatherDashboard: React.FC<WeatherDashboardProps> = ({ cityName, currentWe
     }
   };
 
+  const dewPointMessage = (dewPoint: number) => {
+    if (dewPoint < 10) {
+      return (
+        <div className="text-center">
+          <div className="flex items-center justify-center">
+            <FaTimes className="mr-2" />
+            <span className='text-2xl md:text-4xl'>Avoid Silk Press</span>
+          </div>
+          <p className="text-mustard text-sm mt-3 w-3/5 mx-auto">The air is relatively dry, which can lead to the hair becoming dry and potentially brittle.</p>
+        </div>
+      );
+    } else if (dewPoint >= 10 && dewPoint <= 13) {
+      return (
+        <div className="text-center">
+          <div className="flex items-center justify-center">
+            <FaCheck className="mr-2" />
+            <span>Safe to Silk Press</span>
+          </div>
+          <p className="text-mustard text-sm mt-3 w-3/5 mx-auto">You might need to ensure your hair is adequately moisturized to prevent dryness and brittleness.</p>
+        </div>
+      );
+    } else if (dewPoint >= 14 && dewPoint <= 16) {
+      return (
+        <div className="text-center">
+          <div className="flex items-center justify-center">
+            <FaCheck className="mr-2" />
+            <span>Sweet Spot for Silk Press</span>
+          </div>
+          <p className="text-mustard text-sm mt-3 w-3/5 mx-auto">There's enough moisture in the air to keep your hair hydrated without causing significant frizz.</p>
+        </div>
+      );
+    } else if (dewPoint >= 17 && dewPoint <= 18) {
+      return (
+        <div className="text-center">
+          <div className="flex items-center justify-center">
+            <FaCheck className="mr-2" />
+            <span>Safe with Risk</span>
+          </div>
+          <p className="text-mustard text-sm mt-3 w-3/5 mx-auto">You may need to use anti-humidity products to help maintain the style and reduce the risk of frizz.</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="text-center">
+          <div className="flex items-center justify-center">
+            <FaTimes className="mr-2" />
+            <span>Avoid Silk Press</span>
+          </div>
+        </div>
+      );
+    }
+  };
+
   const currentDewPoint = calculateDewPoint(currentWeather.main.temp, currentWeather.main.humidity);
-  const isSafeToSilkPress = currentDewPoint < 18; // Safe if dew point is less than 18°C
 
   return (
     <div className="flex flex-col justify-between px-5 pt-5 md:px-20">
@@ -107,9 +158,8 @@ const WeatherDashboard: React.FC<WeatherDashboardProps> = ({ cityName, currentWe
 
         {/* Current Weather Info */}
         <div className="flex flex-col items-center justify-around text-2xl font-montserrat capitalize my-5">
-          <p className={`mb-6 text-2xl md:text-4xl ${isSafeToSilkPress ? 'text-green-500' : 'text-red-500'} flex items-center`}>
-            {isSafeToSilkPress ? <FaCheck className="mr-2" /> : <FaTimes className="mr-2" />}
-            {isSafeToSilkPress ? "Safe to Silk Press" : "Avoid Silk Press"}
+          <p className={`mb-6`}>
+            {dewPointMessage(currentDewPoint)}
           </p>
           <p className="text-4xl md:text-6xl mb-5">Dew Point: {getDewPoint(currentWeather.main.temp, currentWeather.main.humidity)}°{unit}</p>
           <p>Temp: {getTemperature(currentWeather.main.temp)}°{unit}</p>
@@ -125,7 +175,7 @@ const WeatherDashboard: React.FC<WeatherDashboardProps> = ({ cityName, currentWe
           <div className="flex flex-wrap justify-around w-full text-xs md:text-sm text-center">
             {forecasts.map((forecast, index) => {
               const forecastDewPoint = calculateDewPoint(forecast.main.temp, forecast.main.humidity);
-              const forecastSafeToSilkPress = forecastDewPoint < 18;
+              const forecastSafeToSilkPress = forecastDewPoint >= 10 && forecastDewPoint <= 18;
 
               return (
                 <div key={index} className="m-2 p-2 w-28 md:w-40 text-white rounded-lg">
