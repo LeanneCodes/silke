f41033e20366812ea40ebe5bdbfd0ea317de4f46
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-07-20 10:36:41 +0100

    add more faqs and categorise them

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -1,18 +1,7 @@
 'use client';
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-
-const faqs = [
-  {
-    question: "Why I created this website?",
-    answer: "I created this website to help women with textured hair determine the best time to get a silk press. Managing textured hair can be challenging, especially with varying weather conditions. This website aims to provide the necessary information to make informed decisions about hair care, particularly when it comes to achieving the perfect silk press."
-  },
-  {
-    question: "Key Points About Weather and Hair Styling",
-    answer: "Understanding the relationship between weather, dew points, and hair styling is crucial for maintaining healthy and beautiful textured hair. Here are some key points: Dew Points, Humectants, Weather Impact, and Product Selection."
-  },
-];
+import React, { useState, useEffect } from 'react';
+import faqs from './faqs.json'
 
 const About: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
@@ -24,23 +13,28 @@ const About: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-montserrat mb-6 text-center">Frequently Asked Questions</h1>
-      <div className="space-y-4">
-        {faqs.map((faq, index) => (
-          <div key={index} className='bg-sage opacity-80 rounded'>
-            <button
-              onClick={() => toggleAnswer(index)}
-              className="w-full text-left py-2 px-4 bg-primary text-white font-montserrat rounded-t"
-            >
-              {faq.question}
-            </button>
-            {activeIndex === index && (
-              <div className="p-4 bg-white text-darkGrey font-montserrat rounded-b">
-                {faq.answer}
+      {Object.entries(faqs).map(([category, faqList]) => (
+        <div key={category} className="mb-8">
+          <h2 className="text-3xl font-montserrat mb-4">{category.replace(/([A-Z])/g, ' $1')}</h2>
+          <div className="space-y-4">
+            {faqList.map((faq, index) => (
+              <div key={index} className='bg-sage opacity-80 rounded'>
+                <button
+                  onClick={() => toggleAnswer(index)}
+                  className="w-full text-left py-2 px-4 bg-primary text-white font-montserrat rounded-t"
+                >
+                  {faq.question}
+                </button>
+                {activeIndex === index && (
+                  <div className="p-4 bg-white text-darkGrey font-montserrat rounded-b">
+                    {faq.answer}
+                  </div>
+                )}
               </div>
-            )}
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
