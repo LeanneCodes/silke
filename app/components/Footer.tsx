6f03be466dfb1fafc07f0527275109d715796f1a
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-07-20 10:21:16 +0100

    adjust footer spacing

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ className }) => {
   return (
-    <footer className={`w-full mt-[25px] text-white py-4 relative bottom-0 ${className}`}>
+    <footer className={`w-full mt-[5px] text-white py-4 relative bottom-0 ${className}`}>
       <div className="container mx-auto text-center">
         <p className='font-montserrat'>&copy; {new Date().getFullYear()} <span className='font-playwrite'>silke</span>. All rights reserved.</p>
       </div>
